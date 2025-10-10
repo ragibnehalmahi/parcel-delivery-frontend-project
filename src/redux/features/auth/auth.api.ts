7ee32dccb4c5524209d134461571b9bf2022e047
Ragib Nehal Mahi <76880336+ragibnehalmahi@users.noreply.github.com>
2025-10-11 00:13:46 +0600

    basic file and folder structure implemented

---
+++ src/redux/features/auth/auth.api.ts
@@ -0,0 +1,45 @@
+import { baseApi } from "@/redux/baseApi";
+ 
+
+export const authApi = baseApi.injectEndpoints({
+  endpoints: (builder) => ({
+    login: builder.mutation({
+      query: (userInfo) => ({
+        url: "/auth/login",
+        method: "POST",
+        data: userInfo,
+      }),
+    }),
+    logout: builder.mutation({
+      query: () => ({
+        url: "/auth/logout",
+        method: "POST",
+      }),
+      invalidatesTags: ["User"]
+    }),
+    register: builder.mutation({
+      query: (userInfo) => ({
+        url: "/user/register",
+        method: "POST",
+        data: userInfo,
+      }),
+    }),
+    
+     
+    userInfo: builder.query({
+      query: () => ({
+        url: "/user/me",
+        method: "GET",
+      }),
+      providesTags: ["User"],
+    }),
+  }),
+});
+
+export const {
+  useRegisterMutation,
+  useLoginMutation,
+   
+  useUserInfoQuery,
+  useLogoutMutation,
+} = authApi;
