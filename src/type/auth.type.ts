2d07b1606a1d61d071bc47324fc8ec10a57b7fd5
Ragib Nehal Mahi <ragibnehal2510@gmail.com>
2025-10-11 09:48:51 +0600

    added auth type

---
+++ src/type/auth.type.ts
@@ -0,0 +1,45 @@
+// src/types/auth.types.ts
+
+import { IUser } from "./user.types";
+
+ 
+
+export interface AuthTokens {
+  accessToken?: string;
+  refreshToken?: string;
+}
+
+export interface LoginRequest {
+  email: string;
+  password: string;
+}
+
+export interface LoginResponse {
+  success: boolean;
+  message?: string;
+  data?: {
+    user: IUser;
+    tokens: AuthTokens;
+  };
+}
+
+export interface RegisterRequest {
+  name: string;
+  email: string;
+  password: string;
+  phone: string;
+  role?: "SENDER" | "RECEIVER";
+}
+
+export interface RefreshTokenResponse {
+  success: boolean;
+  tokens: AuthTokens;
+}
+
+export interface DecodedUser {
+  id: string;
+  email: string;
+  role: string;
+  iat?: number;
+  exp?: number;
+}
