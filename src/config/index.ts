498acb6cb799c24bd62f8a1cff39db3104b0f5a9
Ragib Nehal Mahi <76880336+ragibnehalmahi@users.noreply.github.com>
2025-11-15 22:41:56 +0600

    config folder code updated

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
 const config = {
   baseUrl: 'http://localhost:5000/api/v1/',
 };
 
-=======
-const config = {
-  baseUrl: 'http://localhost:5000/api/v1/',
-};
-
->>>>>>> 83f810d1e4f52bcfb5248d889b25b62f8f7b5a8b
 export default config;
